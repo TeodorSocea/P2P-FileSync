27ad76addb0173ece16765b8e3d9e947994ecabb
lynxkata <catalin_cristian23@yahoo.com>
2022-04-15 19:42:17 +0300

    Command Design Pattern implementat pentru manipularea fisierelor, alaturi de functia pt adaugare de fisiere in functie de OS.

--- src/main/java/Resident_Daemon/Core/Main.java
+++ src/main/java/Resident_Daemon/Core/Main.java
@@ -1,26 +1,16 @@
 package Resident_Daemon.Core;
 
 import Resident_Daemon.CommandsPack.CommandExecutor;
-import Resident_Daemon.CommandsPack.Commands.Command;
+import Resident_Daemon.CommandsPack.Commands.LocalAPI.NewFile;
 import Resident_Daemon.PackageOS.DetectOS;
-import Resident_Daemon.PackageOS.SpecificAPI;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("App started!");
-        String os = new DetectOS().getOperatingSystem();
-        new SpecificAPI().runOSActions(); //detecteaza automat sistemul si face chestiile necesare (initial nu se cunoaste OS-ul)
-        //new SpecificAPI().WindowsCommands(); executa comenzi specifice windows (initial se cunoaste OS-ul)
-        //new SpecificAPI().UnixCommands(); executa comenzi specifice unix (initial se cunoaste OS-ul)
-        //new SpecificAPI().MacOSCommands(); executa comenzi specifice macos (initial se cunoaste OS-ul)
+        new DetectOS().getOperatingSystem();
         CommandExecutor commandExecutor = new CommandExecutor();
-        commandExecutor.ExecuteOperation(new Command() {
-            @Override
-            public boolean execute() {
-                System.out.println("Executed dummy command!");
-                return false;
-            }
-        });
+        commandExecutor.ExecuteOperation(new NewFile()); //detecteaza automat sistemul de operare si creaza un moruz.txt
+
 
     }
 }
---
+++ src/main/java/Resident_Daemon/PackageOS/LocalAPI.java
@@ -0,0 +1,33 @@
+package Resident_Daemon.PackageOS;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class LocalAPI extends DetectOS {
+    public void CreateNewFile(String fileName) {
+        try {
+            String home = System.getProperty("user.home"); //functia asta creaza un fisier nou la adresa de acasa a userului
+            Path path = Paths.get(home, fileName);
+            Files.createDirectories(path.getParent());
+            try {
+                Files.createFile(path);
+            } catch (FileAlreadyExistsException e) {
+                System.err.println("already exists: " + e.getMessage()); //se verifica daca exista deja fisierul
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void WindowsCommands(String fileName) {
+        CreateNewFile(fileName);
+    }
+    public void MacOSCommands(String fileName) {
+        CreateNewFile(fileName);
+    }
+    public void UnixCommands(String fileName) {
+        CreateNewFile(fileName);
+    }
+}
+
