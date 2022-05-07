0a7ea4e1e6c2d08df7fc0a549d03a322a899226c
Silviu-Munteanu <silviu1827@yahoo.com>
2022-05-07 20:39:18 +0300

    Am implementant ChooseFolderTest si am rezolvat toate warningurile la ambele teste pentru ca cei de la RD sunt smecheri.

--- src/main/java/Resident_Daemon/UnitTests/ChooseFileToSyncTest.java
+++ src/main/java/Resident_Daemon/UnitTests/ChooseFileToSyncTest.java
@@ -1,29 +1,28 @@
 package Resident_Daemon.UnitTests;
 
-import Networking.Core.NetworkingComponent;
+
 import Resident_Daemon.BasicFileUtils;
-import Resident_Daemon.Core.Singleton;
+
 import org.junit.jupiter.api.Test;
 
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChooseFileToSyncTest {
 
     @Test
-    void testFileMatching(){
+    void testFileMatching() {
         String folderPath = "src/main/java/Resident_Daemon/UnitTests/Media";
 
         System.out.println("The folder path is: " + folderPath);
 
         String fileRelativePath = "ceva.txt";
-
 
         Path filePath = Paths.get(folderPath, fileRelativePath);
 
@@ -31,9 +30,8 @@ class ChooseFileToSyncTest {
 
         byte[] out = BasicFileUtils.file2bytes(filePath);
         String fileContent = new String(out, StandardCharsets.UTF_8);
-        StringBuilder stringBuilder = new StringBuilder(fileRelativePath + "!" + fileContent);
 
-        String fromFileOLD = stringBuilder.toString();
+        String fromFileOLD = fileRelativePath + "!" + fileContent;
         String dataShouldBe = "ceva.txt!niste bytes obositi";
 
         assertEquals(dataShouldBe, fromFileOLD);
