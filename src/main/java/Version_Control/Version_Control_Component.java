32317fbf068ca09d778ca090df598c142230f9b8
Petcu George <petcu.georgef@gmail.com>
2022-05-15 19:04:55 +0300

    Added init function and ModifiedFiles to version control component

--- src/main/java/Version_Control/Version_Control_Component.java
+++ src/main/java/Version_Control/Version_Control_Component.java
@@ -15,10 +15,11 @@ public class Version_Control_Component {
     ComparatorP2PFiles comparatorul;
     VersionFile fisierVersiuni;
     Version_Control_Component(){
-        versionFileData = """
-{"files":{"primul":{"2022/05/15 01:21":{"added_content":{"0":"Tata","3":"si","4":"pere"},"deleted_content":{"0":"Mama"}}}}}
+        initVersionFileData();
+        fisierVersiuni = new VersionFile(versionFileData);
     }
-    """;
+    Version_Control_Component(String versionFileData){
+        this.versionFileData = versionFileData;
         fisierVersiuni = new VersionFile(versionFileData);
     }
     public void setOriginalFiles(List<Pair<String, String>> originalFiles) {
@@ -32,14 +33,30 @@ public class Version_Control_Component {
         comparatorul = new ComparatorP2PFiles(originalFiles, otherFiles);
        // System.out.println(comparatorul.compare().getKey());
         fisierVersiuni.setVersionFileData(comparatorul.compare().getKey());
+        System.out.println("Fisiere originale inainte de modificare: " );
+        System.out.println(this.originalFiles);
+        System.out.println("Fisiere originale dupa modificare: ");
+        ModifiedFiles modify = new ModifiedFiles();
+        this.originalFiles = modify.buildModifiedFiles(originalFiles, comparatorul.compare().getKey());
+        System.out.println(this.originalFiles);
         System.out.println(fisierVersiuni.getVersionFileData());
     }
-
+    public void initVersionFileData(){
+        versionFileData = """
+                            {
+                              "files": {
+                             
+                              }
+                            }
+                """;
+    }
     public static void main(String[] args) throws IOException {
         //Merge
         /*System.out.println(fileDifferences("Mama\nare\nmere", "Tata\nare\nmere\nsi\npere").getKey().toString());
         System.out.println(fileDifferences("Mama\nare\nmere", "Tata\nare\nmere\nsi\npere").getValue().toString());*/
 
+      /*  Version_Control_Component version = new Version_Control_Component("""
+                {"files":{"primul":{"2022/05/15 16:01":{"added_content":{"0":"Tata","3":"si","4":"pere"},"deleted_content":{"0":"Mama"}}}}}""");*/ //constructor version file data
         Version_Control_Component version = new Version_Control_Component();
         ArrayList<Pair<String, String>> temp2 = new ArrayList<>();
         temp2.add(new Pair<String, String>("primul", "Mama\nare\nmere"));
