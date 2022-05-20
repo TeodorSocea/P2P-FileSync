d61423528b2b0f264430d36ece7bb23d2b3ffdf2
Petcu George <petcu.georgef@gmail.com>
2022-05-20 16:00:41 +0300

    Modified P2PFile

--- src/main/java/Version_Control/FileP2P.java
+++ src/main/java/Version_Control/FileP2P.java
@@ -1,88 +1,35 @@
 package Version_Control;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Objects;
 
 public class FileP2P {
     String fileName;
-    String userName;
     String data = "";
-    int hash;
+    long timestamp;
 
-    public FileP2P(String filename, String username) {
-        this.fileName = filename;
-        this.userName = username;
-        this.hash = hashCode();
-        readFile(filename);
-    }
 
-    public FileP2P(String fileName, String userName, String data) {
-        this.fileName = fileName;
-        this.userName = userName;
-        this.data = data;
-        this.hash = hashCode();
-    }
-
-    public FileP2P(String fileName, String userName, String data, int hash) {
-        this.fileName = fileName;
-        this.userName = userName;
-        this.data = data;
-        this.hash = hash;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        FileP2P fileP2P = (FileP2P) o;
-        return hash == fileP2P.hash && Objects.equals(fileName, fileP2P.fileName) && Objects.equals(userName, fileP2P.userName) && Objects.equals(data, fileP2P.data);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(fileName, userName, data, hash);
-    }
-
-    public boolean binaryCheck() {
-        char ch;
-        for(int i = 0; i < this.data.length(); i++) {
-            ch = this.data.charAt(i);
-            if(ch != '0' && ch != '1')
-                return false;
+    public FileP2P(FileInputStream file, String fileName, long timestamp) throws IOException {
+        int content;
+        while((content = file.read()) != -1) {
+            this.data += (char)content;
         }
-        return true;
+        this.fileName = fileName;
+
+        this.timestamp = timestamp;
     }
 
-    private void readFile(String filename) {
-        try (FileInputStream file = new FileInputStream(new File(filename))) {
-            int content;
-            while((content = file.read()) != -1) {
-                this.data += (char)content;
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     public String getFileName() {
         return this.fileName;
-    }
-
-    public String getUserName() {
-        return this.userName;
     }
 
     public String getData() {
         return this.data;
     }
 
-    public int getHash() {
-        return this.hash;
+    public Long getTimestamp() {
+        return this.timestamp;
     }
 
     public void setData(String data) {
@@ -93,12 +40,5 @@ public class FileP2P {
         this.fileName = fileName;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public void setHash(int hash) {
-        this.hash = hash;
-    }
 
 }
