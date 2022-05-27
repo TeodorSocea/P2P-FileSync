b3251398d34aeefa92dae008d03a284d00d66214
Balan Tudor-Cristian <balanache1234@gmail.com>
2022-05-27 16:09:45 +0300

    Vreau sa fiu sigur ca nu am nici o problema cu git ul

--- src/main/java/Resident_Daemon/Core/Singleton.java
+++ src/main/java/Resident_Daemon/Core/Singleton.java
@@ -10,7 +10,7 @@ import java.nio.file.Path;
 
 public class Singleton
 {
-    // abcd
+    // abcde
     private static final String EXPORT_IMPORT_FOLDER = "data_singleton.txt";
 
     private String currentPath;
