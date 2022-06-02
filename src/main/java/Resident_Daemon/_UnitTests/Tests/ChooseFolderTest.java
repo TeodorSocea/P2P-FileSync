d99af75bf8fa8992396b3971790d34bed6c51434
Nae Theodor-Gabriel <73588948+ntheodorg@users.noreply.github.com>
2022-06-03 00:56:47 +0300

    Added UnitTest on RD Module

--- src/main/java/Resident_Daemon/_UnitTests/Tests/ChooseFolderTest.java
+++ src/main/java/Resident_Daemon/_UnitTests/Tests/ChooseFolderTest.java
@@ -2,27 +2,24 @@ package Resident_Daemon._UnitTests.Tests;
 
 import Resident_Daemon.CommandsPack.CommandExecutor;
 import Resident_Daemon.CommandsPack.Commands.ChooseFolder;
-import Resident_Daemon.Core.Singleton;
 import org.junit.jupiter.api.Test;
-
-import java.nio.file.InvalidPathException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChooseFolderTest {
-
+    String folderPath;
+    CommandExecutor commandExecutor=new CommandExecutor();
     @Test
-    public void Test(){
-        String folderPath = "GetFo";
-        CommandExecutor commandExecutor = Singleton.getSingletonObject().getCommandExecutor();
-        ChooseFolder chooseFolder = new ChooseFolder(folderPath);
-
-        commandExecutor.ExecuteOperation(chooseFolder);
-
-        Exception exception = chooseFolder.getException();
-
-        assertInstanceOf(InvalidPathException.class, exception);
-
+    void ChooseValidPath()
+    {
+        ChooseFolder chooseFolder=new ChooseFolder("src/main/java/Resident_Daemon/_UnitTests/Tests");
+        assertTrue(commandExecutor.ExecuteOperation(chooseFolder));
+    }
+    @Test
+    void ChooseInvalidPath()
+    {
+        ChooseFolder chooseFolder=new ChooseFolder("src/main/java/Resident_Daemon/_UnitTests/Tes");
+        assertFalse( commandExecutor.ExecuteOperation(chooseFolder));
     }
 
 }
