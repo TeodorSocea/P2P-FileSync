c84125a3ca981ccb84a6a756eea59cae445f0e9f
Nae Theodor-Gabriel <73588948+ntheodorg@users.noreply.github.com>
2022-06-05 17:18:09 +0300

    RD_VC Rollback

---
+++ src/main/java/Resident_Daemon/CommandsPack/Commands/Rollback.java
@@ -0,0 +1,60 @@
+package Resident_Daemon.CommandsPack.Commands;
+
+
+import Resident_Daemon.CommandsPack.Command;
+import Resident_Daemon.Core.Singleton;
+import Resident_Daemon.Utils.BasicFileUtils;
+import Resident_Daemon._UnitTests.ExceptionModule;
+import Version_Control.VersionFileParser;
+import Version_Control.Version_Control_Component;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+
+
+public class Rollback extends ExceptionModule implements Command {
+    private String fileName;
+    private long timestamp;
+    private Integer swarmID;
+
+    public Rollback(String fileName, long timestamp, Integer swarmID) {
+        this.fileName = fileName;
+        this.timestamp = timestamp;
+        this.swarmID = swarmID;
+    }
+
+    private void UpdateFile(String fileRelName, String fileData) {
+        try {
+            BasicFileUtils.WriteFileToFolder(swarmID, fileRelName, fileData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void UpdateVersionFile(String fileData) {
+        BasicFileUtils.WriteVersionFile(swarmID, fileData);
+    }
+
+    @Override
+    public boolean execute() {
+
+        Version_Control_Component vc = Singleton.getSingletonObject().getVersion_control_component();
+
+        Path fileAbsolutePath = Path.of(BasicFileUtils.GetAbsolutePath_FromRelative(swarmID, fileName));
+        byte[] fileBytes = BasicFileUtils.file2bytes(fileAbsolutePath);
+        String fileContent = new String(fileBytes, StandardCharsets.UTF_8);
+
+        vc.setRollbackFile(fileContent);
+        vc.setRollbackFileName(fileName);
+        vc.rollbackFile(timestamp);
+
+        UpdateFile(fileName, vc.getRollbackFile());
+        UpdateVersionFile(vc.getVersionFileData());
+
+
+
+
+        return true;
+    }
+}
